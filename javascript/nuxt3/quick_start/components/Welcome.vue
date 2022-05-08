d3a09794a138ad5beb33803f60134283afd0ae37
Futa HIRAKOBA <korosuke613613@gmail.com>
2022-05-08 18:26:45 +0900

    nuxt3: try routing

---
+++ javascript/nuxt3/quick_start/components/Welcome.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <NuxtLogo />
+    <h1>Welcome to Nuxt</h1>
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+}
+
+</style>
---
+++ javascript/nuxt3/quick_start/pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <!--    <NuxtWelcome />-->
+    <Welcome />
+    <NuxtLink to="nuxt_welcome" class="link n-link-base">
+      NuxtWelcome
+    </NuxtLink>
+  </div>
+</template>
+
+<style>
+.container {
+  height: 100vh;
+  width: 100vw;
+  background: rgb(1, 42, 53);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+h1 {
+  color: white;
+  text-align: center;
+  font-family: helvetica, arial, sans-serif;
+  font-weight: 400;
+}
+
+.link {
+  color: white;
+  text-align: center;
+  font-family: helvetica, arial, sans-serif;
+}
+</style>
